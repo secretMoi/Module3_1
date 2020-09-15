9cdd091fb882af22257b4883c6361af162f97c24
Quentin <win.moi@hotmail.com>
2020-09-15 17:23:42 +0200

    Ajoutez des fichiers projet.

---
+++ Module3_1/App_Start/RouteConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Module3_1
+{
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}"); // ignore ces routes
+
+
+			// crée la route avec 2 valeurs vallant 0 si non définie
+			// http://localhost/Soustraire/2/3
+            routes.MapRoute(
+                name: "Soustraire",
+                url: "Soustraire/{valeur1}/{valeur2}",
+                defaults: new { controller = "Calculateur", action = "Soustraire", valeur1 = 0, valeur2 = 0 }
+            );
+
+            // http://localhost/Ajouter/2/3
+			routes.MapRoute(
+				name: "Ajouter",
+				url: "Ajouter/{valeur1}/{valeur2}",
+				defaults: new { controller = "Calculateur", action = "Ajouter", valeur1 = 0, valeur2 = 0 }
+			);
+
+
+			// finir par la route la plus générale
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+		}
+	}
+}
---
+++ Module3_1/Controllers/CalculateurController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Module3_1.Controllers
+{
+    public class CalculateurController : Controller
+    {
+        // GET: Calculateur
+        public string Ajouter(int valeur1, int valeur2)
+        {
+            int resultat = valeur1 + valeur2;
+            return resultat.ToString();
+        }
+
+        public string Soustraire(int valeur1, int valeur2)
+        {
+            int resultat = valeur1 - valeur2;
+            return resultat.ToString();
+        }
+    }
+}
